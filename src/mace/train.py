Running first test models, but with normalisation mistake!

src/mace/train.py
@@ -43,23 +43,30 @@ def train_one_epoch(data_loader, model, DEVICE, optimizer):
         p = p.to(DEVICE) 
         t = t.to(DEVICE)
 
-        n = torch.swapaxes(n,1,2)
+        if t[-1,-1].item() < 0.011003478870511375:    ## only use data with small dt
 
-        n_hat, modstatus = model(n[:,0,:],p,t)        
+            n = torch.swapaxes(n,1,2)
 
-        if modstatus.item() == 4:
-            status += modstatus.item()
+            n_hat, modstatus = model(n[:,0,:],p,t)        
 
-        ## Calculate losses
-        loss  = loss_function(n,n_hat)
-        overall_loss += loss.item()
+            if modstatus.item() == 4:
+                # print('stat4')
+                status += modstatus.item()
 
-        ## Backpropagation
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+            ## Calculate losses
+            loss  = loss_function(n,n_hat)
+            overall_loss += loss.item()
+
+            ## Backpropagation
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+    
+        else:           ## else: skip this data
+            continue
 
     return (overall_loss)/(i+1), status  ## save losses
+            
 
 
 
@@ -70,7 +77,7 @@ def validate_one_epoch(test_loader, model, DEVICE):
 
     with torch.no_grad():
         for i, (n,p,t) in enumerate(test_loader):
-            print('\tbatch',i+1,'/',len(test_loader),end="\r")
+            # print('\tbatch',i+1,'/',len(test_loader),end="\r")
 
             n     = n.to(DEVICE)     ## op een niet-CPU berekenen als dat er is op de device
             p     = p.to(DEVICE) 

src/mace/training.py
@@ -21,10 +21,11 @@ name = dt.datetime.now()
 path = '/STER/silkem/MACE/models/'+str(name)
 
 ## ADJUST THESE PARAMETERS FOR DIFFERENT MODELS
-epochs = 1
-lr = 1.
+epochs = 20
+lr = 1.e-3
 z_dim = 10
-dirname = 'C-short-dtime'
+# dirname = 'C-short-dtime'
+dirname = 'new'
 
 print('------------------------------')
 print('')
@@ -88,10 +89,6 @@ metadata = {'traindir'  : dirname,
 min_max = np.stack((train.mins, train.maxs), axis=1)
 losses = np.stack((np.array(loss_train_all), np.array(loss_test_all)), axis = 1)
 
-json_object = json.dumps(metadata, indent=4)
-with open(path+"/meta.json", "w") as outfile:
-    outfile.write(json_object)
-
 ## Saving all files
 np.save(path+'/minmax', min_max) 
 np.save(path+'/status', np.array(status))
@@ -99,7 +96,11 @@ np.save(path+'/losses', losses)
 plt.savefig(path+'/mse.png')
 torch.save(model.state_dict(),path+'/nn.pt')
 
-print('** ALL DONE! in [min]', round((toc-tic)/60,2))
+json_object = json.dumps(metadata, indent=4)
+with open(path+"/meta.json", "w") as outfile:
+    outfile.write(json_object)
+
+print('** ',name,' ALL DONE! in [min]', round((toc-tic)/60,2))
 
 
 