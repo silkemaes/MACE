73bd1b7bc807ce5aa3c5383e4cdfcb97d7444cfc
Silke Maes <silke.maes@kuleuven.be>
2023-12-07 17:33:30 +0100

    Developing the dataset to train on the 1D-CSE data

--- src/mace/dataset.py
+++ src/mace/dataset.py
@@ -5,6 +5,8 @@ from os import listdir
 from datetime import datetime
 import json
 import torch
+import sys
+
 
 from torch.utils.data    import Dataset, DataLoader
 
@@ -13,7 +15,24 @@ from torch.utils.data    import Dataset, DataLoader
 import plotting     
 import utils
 
+sys.path.append('/STER/silkem/ChemTorch/src')
+import rates as rate
+## get species
+specs, parnt, convs = rate.read_specs_file('C', 16)
+specs_dict = dict()
+idx_specs  = dict()
+for i in range(len(specs)):
+    specs_dict[specs[i]] = i
+    idx_specs[i] = specs[i]
 
+def normalise(x,min,max):
+        # print("Normalising")
+        norm = (x - min)*(1/np.abs( min - max ))
+        # print(x, norm)
+        return norm
+
+
+### ----------------------- ChemPy/ChemTorch models ----------------------- ###
 class ChemTorchMod():
     '''
     Object representing 1 ChemTorch model.
@@ -94,13 +113,6 @@ class Data(Dataset):
             self.rand_idx = rand_idx[N:]
 
 
-    @staticmethod
-    def normalise(x,min,max):
-        # print("Normalising")
-        norm = (x - min)*(1/np.abs( min - max ))
-        # print(x, norm)
-        return norm
-
     def __len__(self):
         return len(self.rand_idx)
 
@@ -120,13 +132,13 @@ class Data(Dataset):
         ## physical parameters
         trans_p = np.empty_like(mod.p)
         for j in range(3):
-            trans_p[j] = Data.normalise(np.log10(mod.p[j]), self.mins[j], self.maxs[j])
-        trans_p[3] = Data.normalise(mod.p[3], self.mins[3], self.maxs[3])
+            trans_p[j] = normalise(np.log10(mod.p[j]), self.mins[j], self.maxs[j])
+        trans_p[3] = normalise(mod.p[3], self.mins[3], self.maxs[3])
 
         ## abundances
         trans_n = np.clip(mod.n, self.cutoff, None)
         trans_n = np.log10(trans_n)
-        trans_n = Data.normalise(trans_n, self.n_min, self.n_max)       ## max boundary = rel. abundance of He
+        trans_n = normalise(trans_n, self.n_min, self.n_max)       ## max boundary = rel. abundance of He
 
         ## timesteps
         ## normaliseren? eens nadenken: JA! --> herschalen
@@ -196,8 +208,229 @@ def get_test_data(dirname, dt_fract):
     return input, dataset, Chempy_mod
 
 
+### ----------------------- 1D CSE models ----------------------- ###
 
-class MyDataset_1Dmodel(Dataset):
+
+
+def generate_random_numbers(n, start, end):
+    return np.random.randint(start, end, size=n)
+
+class CSEdata(Dataset):
+    '''
+    Class to initialise the dataset to train & test emulator
+
+    Get data from textfiles (output CSE model)
+    
+    Preprocess:
+        - set all abundances < cutoff to cutoff
+        - take np.log10 of abudances
+
+    '''
+    def __init__(self,nb_samples, train=True, fraction=0.7, cutoff = 1e-20, scale = 'norm'):
+        loc = '/STER/silkem/MACE/'
+        paths = np.loadtxt(loc+'data/paths_data_C.txt', dtype=str)
+
+        ## select a certain number of paths, given by nb_samples
+        self.idxs = generate_random_numbers(nb_samples, 0, len(paths))
+        self.path = paths[self.idxs]
+
+        ## These values are the results from a search through the full dataset; see 'minmax.json' file
+        self.logρ_min = np.log10(0.008223)
+        self.logρ_max = np.log10(5009000000.0)
+        self.logT_min = np.log10(10.)
+        self.logT_max = np.log10(1851.0)      
+        self.logδ_min = np.log10(0.0)
+
+        self.fraction = fraction
+        self.train = train
+
+        np.random.seed(0)
+        rand_idx = np.random.permutation(len(self.path))
+        N = int(self.fraction*len(self.path))
+        if self.train:
+            self.rand_idx = rand_idx[:N]
+        else:
+            self.rand_idx = rand_idx[N:]
+            
+            
+    def __len__(self):
+        return len(self.path)
+
+    def __getitem__(self, idx):
+
+        idx = self.rand_idx[idx]
+
+        mod = CSEmod(self.path[idx])
+
+        Δt, n, p = mod.split_in_0D()
+
+
+        return 
+
+
+
+class CSEmod():
+    '''
+    Class to initialise the dataset to train & test emulator
+
+    Get data from textfiles (output CSE model)
+    
+    Preprocess:
+        - set all abundances < cutoff to cutoff
+        - take np.log10 of abudances
+
+    '''
+    def __init__(self, path):
+
+        self.path = path[0:-17]
+        abs_path = 'csfrac_smooth.out'
+        phys_path = 'csphyspar_smooth.out'
+        self.name = path[-43:-18]
+        inp_path = 'inputChemistry_'+self.name+'.txt'
+
+        ## retrieve input
+        self.Rstar, self.Tstar, self.Mdot, self.v, self.eps, self.rtol, self.atol = read_input_1Dmodel(self.path[:-26]+inp_path)
+
+        ## retrieve abundances
+        abs = read_data_1Dmodel(self.path+abs_path)
+        self.n = abs
+
+        ## retrieve physical parameters
+        arr = np.loadtxt(self.path+phys_path, skiprows=4, usecols=(0,1,2,3,4))
+        self.radius, self.dens, self.temp, self.Av, self.delta = arr[:,0], arr[:,1], arr[:,2], arr[:,3], arr[:,4]
+        self.time = self.radius/(self.v) 
+
+    def __len__(self):
+        return len(self.time)
+
+    def get_time(self):
+        return self.time
+    
+    def get_phys(self):
+        return self.dens, self.temp, self.delta, self.Av
+    
+    def get_abs(self):
+        return self.n
+    
+    def get_abs_spec(self,spec):
+        i = specs_dict[spec]
+        return self.n.T[i]
+    
+    def get_dens(self):
+        return self.dens
+    
+    def get_temp(self):
+        return self.temp
+
+    def get_Av(self):
+        return self.Av
+    
+    def get_delta(self):
+        return self.delta
+
+    def get_vel(self):
+        return self.v
+    
+    def get_path(self):
+        return self.path
+
+    def get_name(self):
+        return self.name
+    
+    def get_dt(self):
+        return self.time[1:] - self.time[:-1]
+    
+    def split_in_0D(self):
+        Δt = self.get_dt()
+        n_0D = self.n[:-1]
+        p = np.array([self.dens[:-1], self.temp[:-1], self.delta[:-1], self.Av[:-1]])
+        return Δt, n_0D, p.T
+        
+
+
+def read_input_1Dmodel(file_name):
+    with open(file_name) as file:
+        lines = file.readlines()
+        lines = [item.rstrip() for item in lines]
+
+    Rstar = float(lines[3][9:])
+    Tstar = float(lines[4][9:])
+    Mdot  = float(lines[5][8:])     ## Msol/yr
+    v     = float(lines[6][11:])    ## sec
+    eps   = float(lines[8][19:])
+
+    rtol = float(lines[31][7:])
+    atol = float(lines[32][6:])
+
+    return Rstar, Tstar, Mdot, v, eps, rtol, atol
+
+def read_data_1Dmodel(file_name):
+    '''
+    Read data text file of output abundances of 1D CSE models.
+    '''
+    with open(file_name, 'r') as file:
+        dirty = []
+        proper = None
+        for line in file:
+            try:  
+                if len(line) > 1: 
+                    dirty.append([float(el) for el in line.split()])
+            except:
+                if len(dirty) != 0:
+                    dirty = np.array(dirty)[:,1:]
+                    if proper is None:
+                        proper = dirty
+                    else:
+                        proper = np.concatenate((proper, dirty), axis = 1)
+                dirty = []
+    return proper
+
+
+
+def retrieve_file(dir_name):
+    all_paths_C = []
+    all_paths_O = []
+
+    path = '/lhome/silkem/CHEM/'+dir_name+'/'
+    locs = utils.get_files_in(path)
+    
+    for loc in locs:
+        # print(loc[-3:-1])
+        if loc[-3:-1] == 'ep':
+            # print(path+loc+'/models/')
+            path_mods = utils.get_files_in(path+loc+'/models/')
+            for mod in path_mods:
+                if mod[-1] != 't':
+                    if loc[13] == 'O':
+                        all_paths_O.append(path+loc+'/models/'+mod+'/csfrac_smooth.out')
+                    if loc[13] == 'C':
+                        all_paths_C.append(path+loc+'/models/'+mod+'/csfrac_smooth.out')
+    
+    return all_paths_O, all_paths_C
+
+
+def get_dataset(dir, batch_size, kwargs, plot = False, scale = 'norm'):
+    ## Make PyTorch dataset
+    train = MyDataset_1Dmodel_old(dir=dir, scale = scale)
+    test  = MyDataset_1Dmodel_old(dir=dir, scale = scale, train = False)
+    
+    print('Dataset:')
+    print('------------------------------')
+    print('total # of samples:',len(train)+len(test))
+    print('# training samples:',len(train))
+    print('# testing samples: ',len(test) )
+    print('            ratio: ',np.round(len(test)/(len(train)+len(test)),2))
+
+    data_loader = DataLoader(dataset=train, batch_size=batch_size, shuffle=True ,  **kwargs)
+    test_loader = DataLoader(dataset=test , batch_size=len(test) , shuffle=False,  **kwargs)
+
+    if plot == True:
+        print('\nPlotting histrogram of dataset...')
+        plotting.plot_hist(train.df)
+
+    return train, data_loader, test_loader
+
+class MyDataset_1Dmodel_old(Dataset):
     '''
     Class to initialise the dataset to train & test emulator
 
@@ -270,71 +503,3 @@ class MyDataset_1Dmodel(Dataset):
 
     def get_stats(self):
         return self.mean, self.std, self.min, self.max
-
-    
-
-def read_data_1Dmodel(file_name):
-    '''
-    Read data text file of output abundances of 1D CSE models
-    '''
-    with open(file_name, 'r') as file:
-        dirty = []
-        proper = None
-        for line in file:
-            try:  
-                if len(line) > 1: 
-                    dirty.append([float(el) for el in line.split()])
-            except:
-                if len(dirty) != 0:
-                    dirty = np.array(dirty)[:,1:]
-                    if proper is None:
-                        proper = dirty
-                    else:
-                        proper = np.concatenate((proper, dirty), axis = 1)
-                dirty = []
-    return proper
-
-
-
-def retrieve_file(dir_name):
-    all_paths_C = []
-    all_paths_O = []
-
-    path = '/lhome/silkem/CHEM/'+dir_name+'/'
-    locs = utils.get_files_in(path)
-    
-    for loc in locs:
-        # print(loc[-3:-1])
-        if loc[-3:-1] == 'ep':
-            # print(path+loc+'/models/')
-            path_mods = utils.get_files_in(path+loc+'/models/')
-            for mod in path_mods:
-                if mod[-1] != 't':
-                    if loc[13] == 'O':
-                        all_paths_O.append(path+loc+'/models/'+mod+'/csfrac_smooth.out')
-                    if loc[13] == 'C':
-                        all_paths_C.append(path+loc+'/models/'+mod+'/csfrac_smooth.out')
-    
-    return all_paths_O, all_paths_C
-
-
-def get_dataset(dir, batch_size, kwargs, plot = False, scale = 'norm'):
-    ## Make PyTorch dataset
-    train = MyDataset_1Dmodel(dir=dir, scale = scale)
-    test  = MyDataset_1Dmodel(dir=dir, scale = scale, train = False)
-    
-    print('Dataset:')
-    print('------------------------------')
-    print('total # of samples:',len(train)+len(test))
-    print('# training samples:',len(train))
-    print('# testing samples: ',len(test) )
-    print('            ratio: ',np.round(len(test)/(len(train)+len(test)),2))
-
-    data_loader = DataLoader(dataset=train, batch_size=batch_size, shuffle=True ,  **kwargs)
-    test_loader = DataLoader(dataset=test , batch_size=len(test) , shuffle=False,  **kwargs)
-
-    if plot == True:
-        print('\nPlotting histrogram of dataset...')
-        plotting.plot_hist(train.df)
-
-    return train, data_loader, test_loader
