Correcting the evo loss (gradient loss)

src/mace/CSE_0D/loss.py
@@ -3,6 +3,7 @@ import numpy as np
 import utils
 from torch.autograd.functional import jacobian
 from time import time
+import os
 
 class Loss():
     def __init__(self, norm, fract):
@@ -184,6 +185,7 @@ def mse_loss(x, x_hat):
     Return the mean squared loss (MSE) per x_i.
     '''
     loss = (x-x_hat)**2
+    # print(x.shape, x_hat.shape)
     return loss
 
 def rel_loss(x, x_hat):
@@ -197,16 +199,14 @@ def rel_loss(x, x_hat):
 def evo_loss(x,x_hat):
     '''
     Return the relative evolutions loss per x_i.
-    The relative evolutions loss (EVO) is given by ((x_hat-x_0)/x_0 - (x-x_0)/x_0)**2, 
+    The relative evolutions loss (EVO) is given by ((x-x_0) - (x_hat-x_0))**2, 
         where eps makes sure we don't devide by 0.
     '''
 
-    x_0 = x[:-1]  ## Initial abundances
-    x   = x[1:]   ## Abundances to compare with
-    ## Hence, x-x_0 gives the evolution of the abundances
+    x   = x[1:]   ## ignore initial abundances
 
-    eps = 1e-10
-    loss  = ((torch.abs(x_hat-x_0)/(x_0+eps))-(torch.abs(x-x_0)/(x_0+eps)))**2     ## absolute waarden nemen rond x, zodat het niet nog 0 kan worden
+    loss = (torch.gradient(x)[0] - torch.gradient(x_hat[0])[0])**2
+    
     return loss
 
 def idn_loss(x,x_hat,p, model):
@@ -277,7 +277,8 @@ def loss_function(loss_obj, model, x, x_hat,z_hat, p):
     mse = mse/loss_obj.norm['mse']* loss_obj.fract['mse']
     rel = rel/loss_obj.norm['rel']* loss_obj.fract['rel']
     evo = evo/loss_obj.norm['evo']* loss_obj.fract['evo']
-    idn = idn/loss_obj.norm['idn']* loss_obj.fract['idn']
+    if 'idn' in loss_obj.norm:
+        idn = idn/loss_obj.norm['idn']* loss_obj.fract['idn']
 
     return mse, rel, evo, idn, elm
 
@@ -476,15 +477,16 @@ class Loss_analyse():
         self.set_loss(np.load(loc+type+'/mse.npy'), 'mse')
         self.set_loss(np.load(loc+type+'/rel.npy'), 'rel')
         self.set_loss(np.load(loc+type+'/evo.npy'), 'evo')
-        # self.set_loss(np.load(loc+type+'/idn.npy'), 'idn')
+        if os.path.exists(loc+type+'/idn.npy'):
+            self.set_loss(np.load(loc+type+'/idn.npy'), 'idn')
         self.set_tot_loss(np.load(loc+type+'/tot.npy'))
 
         # self.set_idv_loss(np.load(loc+type+'/mse_idv.npy'), 'mse')
         # self.set_idv_loss(np.load(loc+type+'/rel_idv.npy'), 'rel')
         # self.set_idv_loss(np.load(loc+type+'/evo_idv.npy'), 'evo')
 
-        self.set_norm(meta['norm'])
-        self.set_fract(meta['fract'])
+        # self.set_norm(meta['norm'])
+        # self.set_fract(meta['fract'])
 
         self.set_losstype(meta['losstype'])
 

src/mace/CSE_0D/plotting.py
@@ -56,7 +56,7 @@ def plot_loss(train, test, log = True, ylim = False, limits = None, show = False
         handles.append(l_evo)
 
     ## ------------- IDN -------------
-    c_idn = 'limegreen'
+    c_idn = 'forestgreen'
     if 'idn' in train.type:
         ax1.plot(test.get_loss('idn'), ls = '--', marker = 'x', lw = lw, c=c_idn, alpha = a)
         ax1.plot(train.get_loss('idn'), ls = '-', marker = '.', lw = lw, c=c_idn, alpha = a)
@@ -85,10 +85,11 @@ def plot_loss(train, test, log = True, ylim = False, limits = None, show = False
 
     ax1.set_xlabel('epoch')
     ax1.set_ylabel('loss')
+    # ax1.set_xlim([5.5,7.5])
 
     ax1.grid(True, linestyle = '--', linewidth = 0.2)
 
-    ax1.legend(handles=handles,loc = 'lower right')
+    ax1.legend(handles=handles,loc = 'center left')
     
     plt.tight_layout()
 
@@ -100,14 +101,12 @@ def plot_loss(train, test, log = True, ylim = False, limits = None, show = False
 
 
 def get_evo(n, n_hat):
-
-
     n0 = n[:-1]
 
     Δn = np.abs((n[1:]-n0))
     Δn_hat = np.abs((n_hat-n0))
 
-    print(Δn[:,0].shape)
+    # print(Δn[:,0].shape)
 
     # for i in range(len(Δn_un)):
     #     if Δn_un[i] == 0:
@@ -224,7 +223,7 @@ def plot_evo_specs(n, n_hat, ax1,specs, alpha, title = None):
     return
 
 
-def plot_abs(n, n_hat, plots_path,title = '', specs = []):
+def plot_abs(n, n_hat, plots_path,title = '', specs = [], save = True):
 
     a = 0.5
     ms = 2
@@ -254,9 +253,9 @@ def plot_abs(n, n_hat, plots_path,title = '', specs = []):
     if len(specs) != 0:
         for spec in specs:
             idx = specs_dict[spec]
-            ax2.plot(np.abs((n[1:]-n_hat)[:,idx]/n[:-1][:,idx]), '-o', label = spec, ms = ms)
+            ax2.plot(np.abs((n[1:]-n_hat)[:,idx]/n[1:][:,idx]), '-o', label = spec, ms = ms)
     else:
-        ax2.plot(np.abs((n[1:]-n_hat)/(n[:-1])), '-o', alpha = a, ms = ms)
+        ax2.plot(np.abs((n[1:]-n_hat)/(n[1:])), '-o', alpha = a, ms = ms)
     ax2.plot([0,n_hat.shape[0]],[1,1], '--k', lw = 0.5)
 
     ## ------------------- settings -------------------
@@ -278,10 +277,11 @@ def plot_abs(n, n_hat, plots_path,title = '', specs = []):
 
     plt.tight_layout()
 
-    if len(specs) != 0:
-        plt.savefig(plots_path+'abs_specs_'+title+'.png')
-    else:
-        plt.savefig(plots_path+'abs_'+title+'.png')
+    if save == True:
+        if len(specs) != 0:
+            plt.savefig(plots_path+'abs_specs_'+title+'.png')
+        else:
+            plt.savefig(plots_path+'abs_'+title+'.png')
 
     return fig
 

src/mace/CSE_0D/train.py
@@ -1,6 +1,7 @@
-from time       import time
+from time   import time
 from tqdm   import tqdm
 import numpy as np
+import matplotlib.pyplot as plt
 
 import torch
 from torch.optim  import Adam
@@ -58,7 +59,7 @@ def train_one_epoch(data_loader, model, loss_obj, DEVICE, optimizer):
 
     for i, (n,p,dt) in enumerate(data_loader):
 
-        # print('\tbatch',i+1,'/',len(data_loader),end="\r")
+        print('\tbatch',i+1,'/',len(data_loader),end="\r")
         
         n  = n.view(n.shape[1], n.shape[2]).to(DEVICE)     ## op een niet-CPU berekenen als dat er is op de device
         p  = p.view(p.shape[1], p.shape[2]).to(DEVICE) 
@@ -98,7 +99,7 @@ def validate_one_epoch(test_loader, model, loss_obj, DEVICE):
 
     with torch.no_grad():
         for i, (n,p,dt) in enumerate(test_loader):
-            # print('\tbatch',i+1,'/',len(test_loader),end="\r")
+            print('\tbatch',i+1,'/',len(test_loader),end="\r")
 
             n  = n.view(n.shape[1], n.shape[2]).to(DEVICE)     ## op een niet-CPU berekenen als dat er is op de device
             p  = p.view(p.shape[1], p.shape[2]).to(DEVICE) 
@@ -153,6 +154,8 @@ def train(model, lr, data_loader, test_loader, path, end_epochs, DEVICE, trainlo
         ## save model
         if (start_epochs+epoch)%10 == 0 and path != None:
             torch.save(model.state_dict(),path+'/nn/nn'+str(int((epoch)/10))+'.pt')
+            plotting.plot_loss(trainloss, testloss, log = log, show = show)
+            plt.savefig(path+'/loss.png')
         
         print("Epoch", epoch + 1, "complete!", "\tAverage loss train: ", trainloss_dict['tot'], "\tAverage loss test: ", testloss_dict['tot'])
     print('\n \tDONE!')
@@ -167,16 +170,8 @@ def train(model, lr, data_loader, test_loader, path, end_epochs, DEVICE, trainlo
 
 def test(model, input,  loss_obj):
 
-    losses = Loss(None,None)
 
     mace_time = list()
-    loss_dict = dict()
-    loss_dict['mse'] = 0
-    loss_dict['rel'] = 0
-    loss_dict['evo'] = 0
-    loss_dict['idn'] = 0
-    loss_dict['elm'] = 0
-    loss_dict['tot'] = 0
     print('>>> Testing model...')
 
     model.eval()
@@ -187,19 +182,15 @@ def test(model, input,  loss_obj):
     # print(n.shape, p.shape,dt.shape)
     
     tic = time()
-    n_hat,z_hat, modstatus = model(n[:-1],p,dt)    ## Give to the solver abundances[0:k] with k=last-1, without disturbing the batches 
+    n_hat, z_hat, modstatus = model(n[:-1],p,dt)    ## Give to the solver abundances[0:k] with k=last-1, without disturbing the batches 
     toc = time()
-
-    loss,loss_dict = process_loss_one_epoch(loss_dict, n, n_hat, z_hat,p, model, loss_obj)
-    losses.set_loss_all(loss_dict, 1)
 
     solve_time = toc-tic
     mace_time.append(solve_time)
 
-    print('\nTest loss     :',loss.item())
     print('Solving time [s]:', solve_time)
 
-    return n.detach().numpy(), n_hat[0].detach().numpy(), dt, losses, mace_time
+    return n.detach().numpy(), n_hat[0].detach().numpy(), dt, mace_time
 
 def test_evolution(model, input, start_idx):
 
@@ -208,13 +199,14 @@ def test_evolution(model, input, start_idx):
     mace_time = list()
     n_evo = list()
 
-    print('>>> Testing model...')
+    print('\n>>> Testing model...')
 
     model.eval()
     n     = input[0][start_idx]
     p     = input[1]
     dt    = input[2]
 
+    tic_tot = time()
     ## first step of the evolution
     tic = time()
     n_hat, z_hat,modstatus = model(n.view(1, -1),p[start_idx].view(1, -1),dt[start_idx].view(-1))    ## Give to the solver abundances[0:k] with k=last-1, without disturbing the batches 
@@ -223,12 +215,18 @@ def test_evolution(model, input, start_idx):
     solve_time = toc-tic
     mace_time.append(solve_time)
 
+    
     ## subsequent steps of the evolution
     for i in tqdm(range(start_idx+1,len(dt))):
+        tic = time()
         n_hat,z_hat, modstatus = model(n_hat.view(1, -1),p[i].view(1, -1),dt[i].view(-1))    ## Give to the solver abundances[0:k] with k=last-1, without disturbing the batches
+        toc = time()
         n_evo.append(n_hat.detach().numpy())
         solve_time = toc-tic
         mace_time.append(solve_time)
+    toc_tot = time()
 
+    print('Solving time [s]:', np.array(solve_time).sum())
+    print('Total   time [s]:', toc_tot-tic_tot)
 
-    return np.array(n_evo).reshape(-1,468), mace_time
+    return np.array(n_evo).reshape(-1,468), np.array(solve_time)