d8eb7f031ff2a386056079180d96e6bfb866c750
Silke Maes <silke.maes@kuleuven.be>
2024-06-04 14:18:30 +0200

    working on online docs

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -54,9 +54,7 @@ exclude_patterns = ['_build']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'better'
-# html_logo  = 'images/pomme_logo.svg'
-# html_theme_options = {'logo_only': True}
+html_theme = 'press'
 html_theme_options = {
 
 }
