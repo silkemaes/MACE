8a365325245942a36955d9b594db1f67ca61752f
Silke Maes <silke.maes@kuleuven.be>
2024-06-04 14:09:47 +0200

    working on online docs

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -20,7 +20,7 @@ year = datetime.datetime.now().year
 # -- Project information -----------------------------------------------------
 
 project = 'MACE'
-copyright = f'2022-{year}, Silke Maes'
+copyright = f'{year}, Silke Maes'
 author = 'Silke Maes'
 
 # -- Explicitly specify master doc -------------------------------------------
@@ -58,20 +58,7 @@ html_theme = 'better'
 # html_logo  = 'images/pomme_logo.svg'
 # html_theme_options = {'logo_only': True}
 html_theme_options = {
-    'analytics_id': 'G-XXXXXXXXXX',  #  Provided by Google in your dashboard
-    'analytics_anonymize_ip': False,
-    'logo_only': False,
-    'display_version': True,
-    'prev_next_buttons_location': 'bottom',
-    'style_external_links': False,
-    'vcs_pageview_mode': '',
-    'style_nav_header_background': 'white',
-    # Toc options
-    'collapse_navigation': True,
-    'sticky_navigation': True,
-    'navigation_depth': 4,
-    'includehidden': True,
-    'titles_only': False
+
 }
 
 
